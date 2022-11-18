33bc0b5317b58ddb8892defa71292d1a9f17f1fb
Mehdi Saber <m1d3s@protonmail.com>
2022-11-18 14:13:44 +0100

    Exo 5

---
+++ pages/api/socket.js
@@ -0,0 +1,25 @@
+import {Server} from "socket.io";
+
+const SocketHandler = (req, res) => {
+    if (res.socket.server.io) {
+        console.log('Socket is already running')
+    } else {
+        console.log('Socket is initializing')
+        const io = new Server(res.socket.server)
+        res.socket.server.io = io
+
+        io.on('connection', socket => {
+            socket.on('input-submit', (x, y) => {
+                socket.emit('result', x + y)
+            })
+
+            socket.on("message-sent", (message) => {
+                console.log(message)
+
+            })
+        })
+    }
+    res.end()
+}
+
+export default SocketHandler
